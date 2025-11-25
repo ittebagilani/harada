7fccb8c99d88eaa26075e2250462a7d2e9c3cd72
Itteba Gilani <itteba1@gmail.com>
2025-11-25 11:03:35 -0500

    nov25

--- components/dashboard-client.tsx
+++ components/dashboard-client.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import { UserButton } from "@clerk/nextjs";
+
 interface DashboardClientProps {
   userName: string;
 }
 
 export default function DashboardClient({ userName }: DashboardClientProps) {
   return (
-    <div className="flex">
-      <h1 className="text-[200px] font-thin tracking-tight flex mx-auto mt-20">
-        dashboard
-      </h1>
+    <>
+      <div className="flex">
+        {/* <h1 className="text-[200px] font-thin tracking-tight flex mx-auto mt-20">
+          dashboard
+        </h1> */}
 
-      <p className="absolute top-10 left-10 text-lg">
-        Welcome, {userName}
-      </p>
-    </div>
+        <p className="text-lg mx-auto">Welcome, {userName}</p>
+        <div className="">
+
+        <UserButton />
+        </div>
+      </div>
+    </>
   );
 }
